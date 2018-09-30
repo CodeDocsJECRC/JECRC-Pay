dcb5e95a951416d5e3b63cf6a9661b6db612aa41
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2018-09-30 19:11:07 +0530

    variables and paytm details fixed

--- app/src/main/java/com/example/admin/navbottom/Activities/LoginActivity.java
+++ app/src/main/java/com/example/admin/navbottom/Activities/LoginActivity.java
@@ -18,32 +18,32 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth mfirebaseAuth;
     private int counter =5;
-    private EditText e1,e2;
+    private EditText mUsername,mPassword;
     private TextView SIGNUP;
     private Button login;
     private ProgressDialog progressDialog;
-    private TextView t;
+    private TextView mAttemptText;
     String namegive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        e1=(EditText)findViewById(R.id.username);
-        e2=(EditText)findViewById(R.id.password);
+        mUsername=(EditText)findViewById(R.id.username);
+        mPassword=(EditText)findViewById(R.id.password);
         login=(Button)findViewById(R.id.loginn);
-        firebaseAuth= FirebaseAuth.getInstance();
+        mfirebaseAuth= FirebaseAuth.getInstance();
         SIGNUP=(TextView)findViewById(R.id.button2);
-        t=(TextView)findViewById(R.id.textView);
+        mAttemptText=(TextView)findViewById(R.id.textView);
         progressDialog=new ProgressDialog(this);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setIcon(R.mipmap.white);
 
         //to check current user
-        if(firebaseAuth.getCurrentUser()!=null)
+        if(mfirebaseAuth.getCurrentUser()!=null)
         {
             finish();
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
@@ -74,15 +74,15 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     private void validate()
-    {   String  User=e1.getText().toString().trim();
-        String Pass=e2.getText().toString().trim();
+    {   String  User=mUsername.getText().toString().trim();
+        String Pass=mPassword.getText().toString().trim();
 
 
 
         progressDialog.setMessage("Loading...");
         progressDialog.show();
 
-        firebaseAuth.signInWithEmailAndPassword(User,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {    //ADDCLICK ON LISTENER IS USED TO CHECK THAT THE TASK IS SUCCESSFUL OR NOT
+        mfirebaseAuth.signInWithEmailAndPassword(User,Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {    //ADDCLICK ON LISTENER IS USED TO CHECK THAT THE TASK IS SUCCESSFUL OR NOT
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful())
@@ -97,7 +97,7 @@ public class LoginActivity extends AppCompatActivity {
                 {progressDialog.dismiss();
                     Toast.makeText(getApplicationContext(), "LoginActivity Failed!", Toast.LENGTH_SHORT).show();
                     counter--;
-                    t.setText("No. of Attempts Remaining "+ String.valueOf(counter));
+                    mAttemptText.setText("No. of Attempts Remaining "+ String.valueOf(counter));
                     if(counter==0)
                         login.setEnabled(false);
 
